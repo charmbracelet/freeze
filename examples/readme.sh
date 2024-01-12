7d2d7813c7348f454aec59e21476953baeee3373
Maas Lalani <maas@lalani.dev>
2024-01-12 17:25:39 -0500

    fix: add margin to shadow.svg

--- examples/readme.sh
+++ examples/readme.sh
@@ -5,5 +5,5 @@ freeze artichoke.hs --radius 8 --window --output window.svg
 freeze artichoke.hs --radius 8 --window --border --output border.svg
 freeze artichoke.hs --radius 8 --window --border --padding 30,50,30,30 --output padding.svg
 freeze artichoke.hs --radius 8 --window --border --padding 30,50,30,30 --margin 20 --output margin.svg
-freeze artichoke.hs --radius 8 --window --border --padding 20,40,20,20 --shadow --output shadow.svg
+freeze artichoke.hs --radius 8 --window --border --padding 20,40,20,20 --margin 20 --shadow --output shadow.svg
 
