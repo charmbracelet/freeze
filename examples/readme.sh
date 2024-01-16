a753f81f4279b10a64919bb188199743dd862728
Maas Lalani <maas@lalani.dev>
2024-01-16 17:49:46 -0500

    fix: margin

--- examples/readme.sh
+++ examples/readme.sh
@@ -6,6 +6,6 @@ freeze artichoke.hs --border.radius 8 --output corner-radius.svg
 freeze artichoke.hs --border.radius 8 --window --output window.svg
 freeze artichoke.hs --border.radius 8 --window --border.width 1 --output border.svg
 freeze artichoke.hs --border.radius 8 --window --border.width 1 --padding 30,50,30,30 --output padding.svg
-freeze artichoke.hs --border.radius 8 --window --border.width 1 --padding 30,50,30,30 --margin 20 --output margin.svg
-freeze artichoke.hs --border.radius 8 --window --border.width 1 --padding 20,40,20,20 --margin 20 --shadow --output shadow.svg
+freeze artichoke.hs --border.radius 8 --window --border.width 1 --padding 30,50,30,30 --margin 50,60,100,60 --output margin.svg
+freeze artichoke.hs -c full --output shadow.svg
 
