49d07c9ac5f71390d0c5abce8ba2d4bef71109d1
Christian Rocha <christian@rocha.is>
2024-03-25 15:52:46 -0400

    chore(help): general help improvements (#36)
    * chore(help): drop newline under 'customization'
    
    * chore(help): fix colors to values independent of the ANSI 0-16 set
    
    * chore: match 'WROTE' color to help color
    
    * chore(help): improve help on light mode

--- help.go
+++ help.go
@@ -14,13 +14,12 @@ const space = 18
 var highlighter = regexp.MustCompile("{{(.+?)}}")
 
 func helpPrinter(_ kong.HelpOptions, ctx *kong.Context) error {
-	codeBlockStyle := lipgloss.NewStyle().Background(lipgloss.Color("0")).MarginLeft(2).Padding(1, 2)
-	programStyle := lipgloss.NewStyle().Background(codeBlockStyle.GetBackground()).Foreground(lipgloss.Color("12")).PaddingLeft(1)
-	stringStyle := lipgloss.NewStyle().Background(codeBlockStyle.GetBackground()).Foreground(lipgloss.Color("3")).PaddingLeft(1)
-	argumentStyle := lipgloss.NewStyle().Background(codeBlockStyle.GetBackground()).Foreground(lipgloss.Color("7")).PaddingLeft(1)
+	codeBlockStyle := lipgloss.NewStyle().Background(lipgloss.AdaptiveColor{Light: "254", Dark: "235"}).MarginLeft(2).Padding(1, 2)
+	programStyle := lipgloss.NewStyle().Background(codeBlockStyle.GetBackground()).Foreground(lipgloss.Color("#7E65FF")).PaddingLeft(1)
+	stringStyle := lipgloss.NewStyle().Background(codeBlockStyle.GetBackground()).Foreground(lipgloss.Color("#46CB8B")).PaddingLeft(1)
+	argumentStyle := lipgloss.NewStyle().Background(codeBlockStyle.GetBackground()).Foreground(lipgloss.Color("248")).PaddingLeft(1)
 	flagStyle := lipgloss.NewStyle().Background(codeBlockStyle.GetBackground()).Foreground(lipgloss.Color("244")).PaddingLeft(1)
-	titleStyle := lipgloss.NewStyle().Bold(true).Transform(strings.ToUpper).Margin(1, 0, 0, 2).Foreground(lipgloss.Color("#875FFF"))
-	// subtitleStyle := lipgloss.NewStyle().Bold(true).Margin(1, 0, 0, 4).Foreground(lipgloss.Color("7"))
+	titleStyle := lipgloss.NewStyle().Bold(true).Transform(strings.ToUpper).Margin(1, 0, 0, 2).Foreground(lipgloss.Color("#6C50FF"))
 
 	fmt.Println()
 	fmt.Println("  Generate images of code and terminal output. 📸")
@@ -58,7 +57,7 @@ func helpPrinter(_ kong.HelpOptions, ctx *kong.Context) error {
 		}
 	}
 
-	fmt.Println(titleStyle.Render("Customization"))
+	fmt.Print(titleStyle.Render("Customization"))
 
 	for _, f := range flags {
 		if f.Hidden || f.Name == "help" || f.Group.Title == "Settings" {
