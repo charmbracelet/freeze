cc2c63bb3546cc0530041fead9076434a4370899
Ethan Holz <ethan.holz@thoriumworks.com>
2024-06-07 17:06:20 -0400

    chore(nix): update default.nix with new vendorHash

--- default.nix
+++ default.nix
@@ -2,5 +2,5 @@
 pkgs.buildGoModule {
   name = "freeze";
   src = ./.;
-  vendorHash = "sha256-AUFzxmQOb/h0UgcprY09IVI7Auitn3JTDU/ptKicIAU=";
+  vendorHash = "sha256-OFNpZ6BOxC1nVmf0X89wlzIb3S7xS89YX4EkYXF4ozM=";
 }
