25670267b8f8b7ca7220a8232d074ddeeacbb0c9
AlejandroSuero <asuerome@gmail.com>
2024-09-04 21:39:24 +0200

    fix: use previous styles

--- error.go
+++ error.go
@@ -8,7 +8,7 @@ import (
 )
 
 var errorHeader = lipgloss.NewStyle().Foreground(lipgloss.Color("#F1F1F1")).Background(lipgloss.Color("#FF5F87")).Bold(true).Padding(0, 1).Margin(1).MarginLeft(2).SetString("ERROR")
-var errorDetails = lipgloss.NewStyle().Foreground(lipgloss.Color("#757575"))
+var errorDetails = lipgloss.NewStyle().Foreground(lipgloss.Color("#757575")).Margin(0, 0, 1, 2)
 
 func printError(title string, err error) {
 	fmt.Printf("%s\n", lipgloss.JoinHorizontal(lipgloss.Center, errorHeader.String(), title))
