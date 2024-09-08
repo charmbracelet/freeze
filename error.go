fdcd0de234f72494adc420224bc5ae11f43142b8
bashbunni <bunni@bashbunni.dev>
2024-09-07 22:24:34 -0700

    wip: lipgloss weird wrapping with git error output (has \t and \r)

--- error.go
+++ error.go
@@ -3,16 +3,27 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/charmbracelet/lipgloss"
 )
 
-var errorHeader = lipgloss.NewStyle().Foreground(lipgloss.Color("#F1F1F1")).Background(lipgloss.Color("#FF5F87")).Bold(true).Padding(0, 1).Margin(1).MarginLeft(2).SetString("ERROR")
-var errorDetails = lipgloss.NewStyle().Foreground(lipgloss.Color("#757575")).Margin(0, 0, 1, 2)
+var (
+	errorHeader  = lipgloss.NewStyle().Foreground(lipgloss.Color("#F1F1F1")).Background(lipgloss.Color("#FF5F87")).Bold(true).Padding(0, 1).Margin(1).MarginLeft(2).SetString("ERROR")
+	errorDetails = lipgloss.NewStyle().Foreground(lipgloss.Color("#757575"))
+)
 
 func printError(title string, err error) {
-	fmt.Printf("%s\n", lipgloss.JoinHorizontal(lipgloss.Center, errorHeader.String(), title))
-	fmt.Printf("%s\n", errorDetails.Render(err.Error()))
+	// fmt.Println(lipgloss.JoinHorizontal(lipgloss.Center, errorHeader.String(), title))
+	var b strings.Builder
+	e := strings.TrimSpace(err.Error())
+	e = strings.ReplaceAll(e, "\r", "")
+	e = strings.ReplaceAll(e, "\t", "")
+	str := strings.Split(e, "\t")
+	for _, s := range str {
+		b.WriteString(errorDetails.Render(s))
+	}
+	fmt.Println(b.String())
 }
 
 func printErrorFatal(title string, err error) {
