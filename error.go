4a7bfb477edc3a6b6ba6fa6a589234cc1f26f06d
bashbunni <bunni@bashbunni.dev>
2024-10-07 12:56:54 -0700

    refactor(error): add background color for debugging

--- error.go
+++ error.go
@@ -5,16 +5,28 @@ import (
 	"os"
 
 	"github.com/charmbracelet/lipgloss"
+	"github.com/charmbracelet/x/ansi"
 )
 
 var (
-	errorHeader  = lipgloss.NewStyle().Foreground(lipgloss.Color("#F1F1F1")).Background(lipgloss.Color("#FF5F87")).Bold(true).Padding(0, 1).Margin(1).MarginLeft(2).SetString("ERROR")
-	errorDetails = lipgloss.NewStyle().Foreground(lipgloss.Color("#757575")).Margin(0, 0, 1, 2)
+	errorHeader = lipgloss.NewStyle().
+			Foreground(lipgloss.Color("#F1F1F1")).
+			Background(lipgloss.Color("#FF5F87")).
+			Bold(true).
+			Padding(0, 1).
+			Margin(1).
+			MarginLeft(2).
+			SetString("ERROR")
+	errorDetails = lipgloss.NewStyle().
+			Background(lipgloss.Color("52")).
+			Foreground(lipgloss.Color("#757575")).
+			Margin(0, 0, 1, 2)
 )
 
 func printError(title string, err error) {
 	fmt.Println(lipgloss.JoinHorizontal(lipgloss.Center, errorHeader.String(), title))
-	fmt.Println(errorDetails.Render(err.Error()))
+	rendered := errorDetails.Render(err.Error())
+	fmt.Println(ansi.Strip(rendered))
 }
 
 func printErrorFatal(title string, err error) {
