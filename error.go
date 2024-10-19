3ea4087b0f9a48f05a8ea41b5d723fb09b82cc98
bashbunni <bunni@bashbunni.dev>
2024-10-18 21:26:43 -0700

    chore: tidy

--- error.go
+++ error.go
@@ -8,15 +8,22 @@ import (
 )
 
 var (
-	errorHeader  = lipgloss.NewStyle().Foreground(lipgloss.Color("#F1F1F1")).Background(lipgloss.Color("#FF5F87")).Bold(true).Padding(0, 1).Margin(1).MarginLeft(2).SetString("ERROR")
-	errorDetails = lipgloss.NewStyle().Foreground(lipgloss.Color("#757575")).Margin(0, 0, 1, 2)
+	errorHeader = lipgloss.NewStyle().
+			Foreground(lipgloss.Color("#F1F1F1")).
+			Background(lipgloss.Color("#FF5F87")).
+			Bold(true).
+			Padding(0, 1).
+			Margin(1).
+			MarginLeft(2).
+			SetString("ERROR")
+	errorDetails = lipgloss.NewStyle().
+			Foreground(lipgloss.Color("#757575")).
+			Margin(0, 0, 1, 2)
 )
 
 func printError(title string, err error) {
-	fmt.Printf("%s\n", lipgloss.JoinHorizontal(lipgloss.Center, errorHeader.String(), title))
-	rendered := errorDetails.Render(err.Error())
-	fmt.Println(rendered)
-	fmt.Printf("%s\n", rendered)
+	fmt.Println(lipgloss.JoinHorizontal(lipgloss.Center, errorHeader.String(), title))
+	fmt.Println(errorDetails.Render(err.Error()))
 }
 
 func printErrorFatal(title string, err error) {
